add1eb5f015bd4713394cb7be5c5c75e647c7b99
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-02-05 21:09:49 +0530

    #

--- contact.py
+++ contact.py
@@ -8,11 +8,11 @@ def details():
     email = ''
     message = ''
 
+    x=2+4
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
-
     return render_template("contact.html", name=name, email=email, message=message)
 
 if __name__ == '__main__':
