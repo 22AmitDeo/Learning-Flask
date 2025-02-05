b70c5aa64ffe6182f5ca984be3f96a125fcd04b2
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-02-05 14:26:46 +0530

    #

--- bmi.py
+++ bmi.py
@@ -3,7 +3,15 @@ app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def root():
+    bmi=''
+    if request.method=='POST' and 'weight' in request.form:
+        weight=float(request.form.get('weight'))
+        height=float(request.form.get('height'))
+        bmi=calc_bmi(weight,height)
 
-    return 'hello'
 
+    return render_template("bmiIndex.html",bmi=bmi)
+
+def calc_bmi(weight,height):
+    return round((weight/(height/100)**2),2)
 app.run()
